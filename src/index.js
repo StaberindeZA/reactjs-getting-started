5612ff82956817b30d418bead92e133e8f137712
root <root@DESKTOP-7K7MUKB.localdomain>
2018-03-09 08:43:33 -0500

    Adding changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,127 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+function formatName(user){
+  return user.firstName + ' ' + user.lastName;
+}
+
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+/*
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+}
+
+const element = (
+  <h1>Hello, {formatName(user)}!</h1>
+);
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);  */
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarURL}
+      alt={props.user.name}
+    />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarURL: 'http://placekitten.com/g/64/64',
+  },
+};
+
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
